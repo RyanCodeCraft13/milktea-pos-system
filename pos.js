fe939f143cbfe4d77e4ab84c9a371816d32b9576
Ryan Christian Noble <154901707+RyanCodeCraft13@users.noreply.github.com>
2026-02-09 21:29:29 +0800

    Update pos.js

--- pos.js
+++ pos.js
@@ -1,4 +1,5 @@
 import { db } from "./firebase.js";
+
 import {
 collection,
 addDoc,
@@ -8,52 +9,57 @@ getDocs
 let cart=[];
 let total=0;
 
-window.addProduct = async function(){
-let name=document.getElementById("name").value;
-let price=parseFloat(document.getElementById("price").value);
-let stock=parseInt(document.getElementById("stock").value);
+window.addProduct=async function(){
+
+let name=name.value;
+let price=parseFloat(price.value);
+let stock=parseInt(stock.value);
 
 await addDoc(collection(db,"products"),{
 name,price,stock
 });
 
-alert("Product Added");
 loadProducts();
 }
 
 async function loadProducts(){
+
 let list=document.getElementById("list");
 if(!list) return;
 
 list.innerHTML="";
+
 const querySnapshot=await getDocs(collection(db,"products"));
 
 querySnapshot.forEach(doc=>{
 let li=document.createElement("li");
-li.textContent=doc.data().name+" ₱"+doc.data().price+" Stock:"+doc.data().stock;
+li.textContent=doc.data().name+" ₱"+doc.data().price;
 list.appendChild(li);
 });
 }
 
 window.addCart=function(){
-let name=document.getElementById("product").value;
-let price=parseFloat(document.getElementById("price").value);
 
-cart.push({name,price});
-total+=price;
+let p=product.value;
+let pr=parseFloat(price.value);
+
+cart.push({p,pr});
+total+=pr;
 
 renderCart();
 }
 
 function renderCart(){
-let cartUI=document.getElementById("cart");
-if(!cartUI) return;
 
-cartUI.innerHTML="";
+let ui=document.getElementById("cart");
+if(!ui) return;
+
+ui.innerHTML="";
+
 cart.forEach(i=>{
 let li=document.createElement("li");
-li.textContent=i.name+" ₱"+i.price;
-cartUI.appendChild(li);
+li.textContent=i.p+" ₱"+i.pr;
+ui.appendChild(li);
 });
 
 document.getElementById("total").innerText=total;
